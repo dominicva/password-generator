60dae7a838a3e409fe8900d98a37fdeee4893830
Dominic van Almsick <dominicvana@gmail.com>
2023-05-10 12:56:42 +0100

    feat: copy to clipboard success

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
-const rangeInput = document.getElementById('character-length');
-const rangeOutputElement = document.getElementById('character-length-value');
+const textElemToCopy = document.getElementById("text-to-copy");
+const copyButton = document.getElementById("copy-to-clipboard");
+const successMessage = document.getElementById("copy-success-message");
+const rangeInput = document.getElementById("character-length");
+const rangeOutput = document.getElementById("character-length-value");
 
-rangeInput.addEventListener('input', e => {
-  rangeOutputElement.textContent = e.target.value;
+copyButton.addEventListener("click", () => {
+  const textToCopy = textElemToCopy.textContent;
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      successMessage.style.transition =
+        "opacity 0.2s cubic-bezier(0.5, 0, 0.5, 1)";
+      successMessage.style.opacity = 1;
+
+      setTimeout(() => {
+        successMessage.style.transition = "none";
+        successMessage.style.opacity = 0;
+      }, 1200);
+    })
+    .catch(e => console.error(e));
+});
+
+rangeInput.addEventListener("input", e => {
+  rangeOutput.textContent = e.target.value;
 });
