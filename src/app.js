db68c6636828f5e46b2f2055f565d67abdde066f
Dominic van Almsick <dominicvana@gmail.com>
2023-05-10 18:01:17 +0100

    feat: form submit error handling

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const successMessage = document.getElementById("copy-success-message");
 const rangeInput = document.getElementById("character-length");
 const rangeOutput = document.getElementById("character-length-value");
 const form = document.querySelector("form");
+const errorMessage = form.querySelector(".error-message");
+const checkboxInputs = form.querySelectorAll('input[type="checkbox"]');
 
 copyButton.addEventListener("click", () => {
   const textToCopy = passwordElem.textContent;
@@ -28,10 +30,27 @@ rangeInput.addEventListener("input", e => {
   rangeOutput.textContent = e.target.value;
 });
 
+for (checkboxInput of checkboxInputs) {
+  checkboxInput.addEventListener("click", () => {
+    errorMessage.style.display = "none";
+  });
+}
+
 form.addEventListener("submit", e => {
   e.preventDefault();
+  errorMessage.style.display = "none";
 
   const inputs = Array.from(e.target.elements).slice(0, -1);
+
+  const hasCheckedInput = inputs.slice(1).reduce((output, currInput) => {
+    if (currInput.checked) return true;
+    else return output;
+  }, false);
+
+  if (!hasCheckedInput) {
+    errorMessage.style.display = "block";
+    return;
+  }
   const passwordOptions = {};
 
   for (const input of inputs) {
@@ -44,6 +63,4 @@ form.addEventListener("submit", e => {
 
   const password = generatePassword(passwordOptions);
   passwordElem.textContent = password;
-
-  console.log(password);
 });
